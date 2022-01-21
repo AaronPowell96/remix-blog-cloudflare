0f8c4260bb7ae0a013ebd2617e12b4d8974af3b6
Aaron Powell <Apowell829@gmail.com>
2022-01-21 20:39:06 +0000

    wrangler deploy

--- worker/index.js
+++ worker/index.js
@@ -6,7 +6,7 @@ import * as build from "../build";
 const handler = createEventHandler({ build });
 const handleFetch = (event) => {
   const url = new URL(event.request.url);
-  if (url.hostname === "rmx.fyi") {
+  if (url.hostname === "remix-blog-cloudflare.pages.dev") {
     event.respondWith(redirectHandler(event));
     return;
   }
